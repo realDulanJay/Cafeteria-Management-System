47ed1c24d916f876bb2222a7702e412e7737c0fe
Dulan Jayawickrama <jayawickramadulan@gmail.com>
2024-01-09 22:00:29 +0530

    Update home component

--- Cafeteria-Management-System-Frontend/src/app/app.module.ts
+++ Cafeteria-Management-System-Frontend/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material-module';
 import { HomeComponent } from './home/home.component';
-import { BestSellerComponent } from './best-seller/best-seller.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SharedModule } from './shared/shared.module';
@@ -24,7 +22,6 @@ import { TokenInterceptorInterceptor } from './services/token-interceptor.interc
   declarations: [
     AppComponent,
     HomeComponent,
-    BestSellerComponent,
     FullComponent,
     AppHeaderComponent,
     AppSidebarComponent,
