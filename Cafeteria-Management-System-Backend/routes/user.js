0376069fc969d49133cfb915d740549b191acc40
Dulan Jayawickrama <jayawickramadulan@gmail.com>
2024-01-12 01:05:40 +0530

    Add delete user option

--- Cafeteria-Management-System-Backend/routes/user.js
+++ Cafeteria-Management-System-Backend/routes/user.js
@@ -178,4 +178,20 @@ router.patch('/updateContactNumber', auth.authenticateToken, checkRole.checkRole
     })
 })
 
+router.delete('/delete/:id', auth.authenticateToken, checkRole.checkRole, (req, res, next) => {
+    const id = req.params.id;
+    var query = "delete from user where id = ?";
+    connection.query(query, [id], (err, results) => {
+        if (!err) {
+            if (results.affectedRows == 0) {
+                return res.status(404).json({ message: "User id does not found" });
+            }
+            return res.status(200).json({ message: "User Deleted Successfully" });
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
 module.exports = router;
